840f717e88bf00e45b188d397dc33d8933fd917e
Satvik12221as <sanu8800971634@gmail.com>
2026-02-25 22:12:47 +0530

    api fixed

--- bwa_frontend.py
+++ bwa_frontend.py
@@ -88,9 +88,9 @@ def extract_latest_state(current_state: Dict[str, Any], step_payload: Any) -> Di
     return current_state
 
 
-# -----------------------------
+# ------------------------------
 # Markdown renderer that supports local images
-# -----------------------------
+# ------------------------------
 _MD_IMG_RE = re.compile(r"!\[(?P<alt>[^\]]*)\]\((?P<src>[^)]+)\)")
 _CAPTION_LINE_RE = re.compile(r"^\*(?P<cap>.+)\*$")
 
